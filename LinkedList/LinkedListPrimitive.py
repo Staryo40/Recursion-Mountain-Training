da6bdec370aef60cf03a670b48104b7722d1faed
Staryo40 <arrrryow@gmail.com>
2025-01-24 19:43:49 +0700

    update: linked list primitive delete first and last

--- LinkedList/LinkedListPrimitive.py
+++ LinkedList/LinkedListPrimitive.py
@@ -3,6 +3,22 @@ class LinkedList:
         self.info = info
         self.next = next
     
+    def length(self):
+        if (self.info == None):
+            return 0
+        if (self.next == None):
+            return 1
+        return 1 + self.next.length()
+
+    def printList(self):
+        if (self.info != None):
+            if (self.next != None):
+                print(self.info, end=" ")
+                print("->", end=" ")
+                self.next.printList()
+            else:
+                print(self.info)
+
     def appendList(self, value):
         if (self.info == None):
             self.info = value
@@ -38,21 +54,35 @@ class LinkedList:
                 else:
                     self.next.insertAt(value, index-1)
 
-    def length(self):
-        if (self.info == None):
-            return 0
+    def deleteFirst(self):
+        value = self.info
         if (self.next == None):
-            return 1
-        return 1 + self.next.length()
-
-    def printList(self):
-        if (self.info != None):
-            if (self.next != None):
-                print(self.info, end=" ")
-                print("->", end=" ")
-                self.next.printList()
+            self.info = None
+        else:
+            if (self.next.next == None):
+                secondLast = self
+                secondLast.info = secondLast.next.info
+                secondLast.next.info = None
+                secondLast.next = None
             else:
-                print(self.info)
+                self.info = self.next.info
+                self.next.deleteFirst()
+        return value
+    
+    def deleteLast(self):
+        if (self.next == None):
+            value = self.info
+            self.info = None
+            return value
+        else:
+            if (self.next.next == None):
+                secondLast = self
+                value = secondLast.next.info
+                secondLast.next.info = None
+                secondLast.next = None
+                return value
+            else:
+                return self.next.deleteLast()
                 
 
 l = LinkedList(1)
@@ -61,5 +91,16 @@ l.appendList(2)
 l.printList()
 l.prependList(0)
 l.printList()
-l.insertAt(7, 0)
+l.insertAt(7, 2)
 l.printList()
+l.deleteFirst()
+l.printList()
+x = l.deleteLast()
+l.printList()
+print(f"x value = {x}")
+x = l.deleteLast()
+l.printList()
+print(f"x value = {x}")
+x = l.deleteLast()
+l.printList()
+print(f"x value = {x}")
