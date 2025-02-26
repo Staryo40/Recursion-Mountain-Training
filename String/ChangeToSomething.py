1e8c417e9006814e172fb8512bf72db2879b93b1
Staryo40 <arrrryow@gmail.com>
2025-02-26 20:23:49 +0700

    feat: change to something

---
+++ String/ChangeToSomething.py
@@ -0,0 +1,8 @@
+def isPalindrome(x:str) -> bool:
+    if (len(x) <= 1):
+        return True
+    if (len(x) == 2):
+        return x[0] == x[1]
+    
+    compare = x[0] == x[len(x)-1]
+    return compare and isPalindrome(x[1:(len(x)-1)])
